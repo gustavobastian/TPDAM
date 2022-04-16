02964508535d6e269ec87c2984010f9ad0c60244
Gustavo Bastian <bastian.gustavo@gmail.com>
2022-04-16 14:10:41 -0300

    add endpoint for retrieving one device info

--- src/backend/routes/dispositivo/index.js
+++ src/backend/routes/dispositivo/index.js
@@ -2,9 +2,26 @@ var express = require('express');
 var routerDispositivo = express.Router();
 var pool = require('../../mysql');
 
-//Devuelve un array de dispositivos
+
+/**
+ * Funcion que envia al front end todos los dispositivos
+ *  
+ * 
+ */
+
 routerDispositivo.get('/', function(req, res) {
     pool.query('Select * from Dispositivos', function(err, result, fields) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result);
+    });
+});
+
+//Espera recibir por parámetro un id de dispositivo y devuelve su informacion
+routerDispositivo.get('/:idDispositivo', function(req, res) {
+    pool.query('Select * from Dispositivos where dispositivoId=?', [req.params.idDispositivo], function(err, result, fields) {
         if (err) {
             res.send(err).status(400);
             return;
