8acd3f8d76792cee00a15bb14031ba4a058e84b2
Gustavo Bastian <bastian.gustavo@gmail.com>
2022-04-26 20:07:44 -0300

    modifications in mediciones  endpoint POST method

--- src/backend/routes/medicion/index.js
+++ src/backend/routes/medicion/index.js
@@ -25,7 +25,7 @@ routerMedicion.get('/:idDispositivo/todas', function(req, res) {
 });
 
 //Espera recibir por parámetro un id de dispositivo y un valor de medición y lo inserta en base de datos.
-routerMedicion.post('/agregar', function(req, res) {
+routerMedicion.post('/', function(req, res) {
     console.log("guardo Medicion");
     console.log(req.body[0]);
     pool.query('Insert into Mediciones (fecha,valor,dispositivoId) values (now(),?,?)', [ req.body.valor, req.body.dispositivoId], function(err, result, fields) {
